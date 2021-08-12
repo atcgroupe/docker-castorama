c16863ed787c00df90b51d4079e037fc096e0970
Pierre Gaimard <pierre@gaimard-family.fr>
2021-08-12 17:13:41 +0200

    Add AppAuthenticator class

---
+++ src/Security/AppAuthenticator.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
+
+class AppAuthenticator extends AbstractLoginFormAuthenticator
+{
+    use TargetPathTrait;
+
+    public const LOGIN_ROUTE = 'app_login';
+    public const LOGIN_ROUTE_CHOICE = 'app_login_choice';
+
+    /*
+     * /login/{type} for app_login route
+     *
+     * Used in AppLogoutListener
+     */
+    public const LOGIN_TYPE_CUSTOMER_SHOP = 'shop';
+    public const LOGIN_TYPE_CUSTOMER_ADMIN = 'admin';
+    public const LOGIN_TYPE_COMPANY_ADMIN = 'atc';
+
+    private UrlGeneratorInterface $urlGenerator;
+
+    public function __construct(UrlGeneratorInterface $urlGenerator)
+    {
+        $this->urlGenerator = $urlGenerator;
+    }
+
+    public function authenticate(Request $request): PassportInterface
+    {
+        $username = $request->request->get('username', '');
+
+        $request->getSession()->set(Security::LAST_USERNAME, $username);
+
+        return new Passport(
+            new UserBadge($username),
+            new PasswordCredentials($request->request->get('password', '')),
+            [
+                new CsrfTokenBadge('authenticate', $request->get('_csrf_token')),
+            ]
+        );
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    {
+        if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
+            return new RedirectResponse($targetPath);
+        }
+
+        return new RedirectResponse($this->urlGenerator->generate('home'));
+    }
+
+    protected function getLoginUrl(Request $request): string
+    {
+        if ($request->attributes->get('_route') !== self::LOGIN_ROUTE) {
+            return $this->urlGenerator->generate(self::LOGIN_ROUTE_CHOICE);
+        }
+
+        $type = $request->attributes->get('type');
+
+        return $this->urlGenerator->generate(self::LOGIN_ROUTE, ['type' => $type]);
+    }
+}
