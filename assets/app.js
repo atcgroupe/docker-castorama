bca6a37370a07cead919d1146bbec9c2915b45d3
Pierre Gaimard <pierre@gaimard-family.fr>
2021-08-25 17:28:07 +0200

    Refactor app.js main entry point

--- assets/app.js
+++ assets/app.js
@@ -1,14 +1,11 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
-// any CSS you import will output into a single css file (app.css in this case)
+/* App scss main file */
 import './styles/app.scss';
 
+/* Vendor libs */
 require('bootstrap');
 
+/* App Scripts */
+require('./scripts/app/app_alerts');
+
 // start the Stimulus application
-import './bootstrap';
+//import './bootstrap';
