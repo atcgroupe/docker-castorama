fdb465ecee3c0835ea22ad7714fb3c51c935d0ad
Pierre Gaimard <pierre@gaimard-family.fr>
2021-08-26 23:00:49 +0200

    Add new entryPoint to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    .addEntry('adminShopList', './assets/scripts/admin/shop_list.js')
 
     .copyFiles({
         from: './assets/images',
