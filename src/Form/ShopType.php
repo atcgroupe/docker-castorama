4c36ff2bb3e56b8ef6273ff0616dfcb7b975bcf6
Pierre Gaimard <pierre@gaimard-family.fr>
2021-08-26 22:55:28 +0200

    Add ShopType FormType

---
+++ src/Form/ShopType.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Shop;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ShopType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add(
+                'name',
+                TextType::class,
+                [
+                    'label' => 'Intitulé du magasin',
+                    'help' => 'Nom réel utilisé pour l\'adresse d\'éxpédition'
+                ]
+            )
+            ->add(
+                'address',
+                TextareaType::class,
+                [
+                    'label' => 'Adresse',
+                    'attr' => [
+                        'rows' => 3,
+                    ]
+                ]
+            )
+            ->add('postCode', TextType::class, ['label' => 'Code postal'])
+            ->add('region', TextType::class, ['label' => 'Région'])
+            ->add('city', TextType::class, ['label' => 'Ville'])
+            ->add(
+                'deliveryInfo',
+                TextareaType::class,
+                [
+                    'label' => 'Infos de livraison',
+                    'attr' => [
+                        'rows' => 5
+                    ]
+                ]
+            )
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Shop::class,
+        ]);
+    }
+}
