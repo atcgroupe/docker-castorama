f90d58df9fc846ed62a065e9a1ce1276924d802a
Pierre Gaimard <pierre@gaimard-family.fr>
2021-08-26 22:55:17 +0200

    Add UserType FormType

---
+++ src/Form/UserType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add(
+                'username',
+                TextType::class,
+                [
+                    'label' => 'Nom du magasin',
+                    'help' => 'Nom utilisé pour le login et l\'affichage'
+                ]
+            )
+            ->add('plainPassword', PasswordType::class)
+            ->add('shop', ShopType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
