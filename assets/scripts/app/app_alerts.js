93253d9814bc057c0686d953124e1f1d63cc9111
Pierre Gaimard <pierre@gaimard-family.fr>
2021-08-30 14:50:41 +0200

    Fix code style in app_alerts entry point

--- assets/scripts/app/app_alerts.js
+++ assets/scripts/app/app_alerts.js
@@ -5,7 +5,7 @@ import { Toast } from "bootstrap";
 Array.from(document.getElementsByClassName('toast')).forEach((toastElm) => {
     const delay = Number(toastElm.dataset.delay);
     const autohide = Boolean(toastElm.dataset.autohide);
-    const toast = new Toast(toastElm, {'autohide': autohide, 'delay': delay})
+    const toast = new Toast(toastElm, {'autohide': autohide, 'delay': delay});
 
     toast.show();
 });
