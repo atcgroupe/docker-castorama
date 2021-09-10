a7b0c117a73d37edcddf96aac311e33425b925cc
Pierre Gaimard <pierre@gaimard-family.fr>
2021-09-10 09:15:48 +0200

    Add type attribute info to SignFixtures

--- src/DataFixtures/SignFixtures.php
+++ src/DataFixtures/SignFixtures.php
@@ -17,12 +17,14 @@ class SignFixtures extends Fixture
     private const WEIGHT = 'weight';
     private const BUILDER = 'builder';
     private const TEMPLATE = 'template';
+    private const TYPE = 'type';
 
     public function load(ObjectManager $manager)
     {
         $data = [
             [
                 self::CLASS_NAME => AisleOrderSign::class,
+                self::TYPE => 'aisle',
                 self::IMAGE => 'aisle_order_sign.jpg',
                 self::TITLE => 'Panneau allée',
                 self::DESCRIPTION => 'Format: 800x500mm<br>Impression: Recto/Verso<br>Matière: PVC 5MM',
@@ -32,6 +34,7 @@ class SignFixtures extends Fixture
             ],
             [
                 self::CLASS_NAME => SectorOrderSign::class,
+                self::TYPE => 'sector',
                 self::IMAGE => 'sector_order_sign.jpg',
                 self::TITLE => 'Panneau secteur',
                 self::DESCRIPTION => 'Format: 1600x500mm<br>Impression: Recto/Verso<br>Matière: PVC 10MM',
@@ -45,6 +48,7 @@ class SignFixtures extends Fixture
             $sign = new Sign();
 
             $sign->setClass($entry[self::CLASS_NAME]);
+            $sign->setType($entry[self::TYPE]);
             $sign->setImage($entry[self::IMAGE]);
             $sign->setTitle($entry[self::TITLE]);
             $sign->setDescription($entry[self::DESCRIPTION]);
@@ -54,6 +58,8 @@ class SignFixtures extends Fixture
             $sign->setIsActive(true);
 
             $manager->persist($sign);
+
+            $this->setReference($entry[self::CLASS_NAME], $sign);
         }
 
         $manager->flush();
