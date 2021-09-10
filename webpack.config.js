f0207fc269a460010434f5b9f5ffb8c26c359bc9
Pierre Gaimard <pierre@gaimard-family.fr>
2021-09-10 09:05:07 +0200

    Add aisleSignEdit entry point to Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,7 @@ Encore
     .addEntry('app', './assets/app.js')
     .addEntry('adminShopList', './assets/scripts/admin/shop_list.js')
     .addEntry('orderList', './assets/scripts/order/order_list.js')
+    .addEntry('aisleSignEdit', './assets/scripts/sign/aisle_sign.js')
 
     .copyFiles({
         from: './assets/images',
