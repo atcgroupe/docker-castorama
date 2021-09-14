1565ce753992ecbbdd2edb42823274d0912b4c5d
Pierre Gaimard <pierre@gaimard-family.fr>
2021-09-14 15:10:43 +0200

    Add new entry point to WebPack config

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,7 @@ Encore
     .addEntry('orderList', './assets/scripts/order/order_list.js')
     .addEntry('aisleSignEdit', './assets/scripts/sign/aisle_sign.js')
     .addEntry('signQuantity', './assets/scripts/sign/sign_quantity.js')
+    .addEntry('sectorSignEdit', './assets/scripts/sign/sector_sign.js')
 
     .copyFiles({
         from: './assets/images',
