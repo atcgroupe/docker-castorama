1e8278b2bba5668ee78ba1409b486e57ed58fe21
Pierre Gaimard <pierre@gaimard-family.fr>
2021-09-15 17:21:49 +0200

    Refactor getOrderSignResume method in OrderSignHelper

--- src/Service/Order/OrderSignHelper.php
+++ src/Service/Order/OrderSignHelper.php
@@ -2,6 +2,7 @@
 
 namespace App\Service\Order;
 
+use App\Entity\AbstractOrderSign;
 use App\Entity\Order;
 use App\Entity\Sign;
 use Doctrine\ORM\EntityManagerInterface;
@@ -38,17 +39,35 @@ class OrderSignHelper
      */
     public function getOrderSignsResume(Order $order): array | null
     {
-        $resume = [];
-        $total = 0;
+        $signResume = [];
+        $totalPrice = 0;
+        $totalCount = 0;
+
         foreach ($this->getSignsTypes() as $type) {
             $repository = $this->manager->getRepository($type->getClass());
-            $count = $repository->count(['order' => $order]);
-
-            $resume[$type->getTitle()] = $count;
-            $total += $count;
+            $count = $this->countSigns($repository->findBy(['order' => $order]));
+            $price = $type->getPrice();
+            $countPrice = $count * $price;
+            $totalCount += $count;
+            $totalPrice += $countPrice;
+            if ($count > 0) {
+                $signResume[$type->getTitle()] = [
+                    'count' => $count,
+                    'unitPrice' => $type->getPrice(),
+                    'countPrice' => $type->getPrice() * $count
+                ];
+            }
         }
 
-        return ($total > 0 ) ? $resume : null;
+        $resume = [
+            'global' => [
+                'count' => $totalCount,
+                'price' => $totalPrice,
+            ],
+            'sign' => $signResume
+        ];
+
+        return ($totalCount > 0 ) ? $resume : null;
     }
 
     /**
@@ -57,5 +76,18 @@ class OrderSignHelper
     private function getSignsTypes(): array
     {
         return $this->manager->getRepository(Sign::class)->findAll();
+    }
+
+    /**
+     * @param AbstractOrderSign[] $signs
+     */
+    private function countSigns(array $signs)
+    {
+        $count = 0;
+        foreach ($signs as $sign) {
+            $count += $sign->getQuantity();
+        }
+
+        return $count;
     }
 }
