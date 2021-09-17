356bf0802d9de87fea983dd3c721a824095b7845
Pierre Gaimard <pierre@gaimard-family.fr>
2021-09-17 10:15:58 +0200

    Add new events to OrderEvent

--- src/Service/Event/OrderEvent.php
+++ src/Service/Event/OrderEvent.php
@@ -8,12 +8,14 @@ use Symfony\Contracts\EventDispatcher\Event;
 class OrderEvent extends Event
 {
     public const CREATED = 'order.created';
+    public const SEND = 'order.send';
     public const SENT = 'order.sent';
     public const RECEIVED = 'order.received';
     public const PROCESS = 'order.process';
     public const PROCESSED = 'order.processed';
     public const SHIPPED = 'order.shipped';
     public const DELIVERED = 'order.delivered';
+    public const STATUS_CHANGED = 'order.status.changed';
 
     protected Order $order;
 
