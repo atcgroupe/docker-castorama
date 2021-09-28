e0dbc76872d3abd6ea485025a6b01105c39cb8a0
Pierre Gaimard <pierre@gaimard-family.fr>
2021-09-28 16:55:05 +0200

    Add missing annotation in AisleSmallOrderSign entity

--- src/Entity/AisleSmallOrderSign.php
+++ src/Entity/AisleSmallOrderSign.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass=AisleSmallOrderSignRepository::class)
+ * @ORM\HasLifecycleCallbacks()
  */
 class AisleSmallOrderSign extends AbstractAisleOrderSign
 {
