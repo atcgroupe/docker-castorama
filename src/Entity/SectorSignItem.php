ef32e7ed48e62f53e9ba15648a0f2314e37199f4
Pierre Gaimard <pierre@gaimard-family.fr>
2021-09-29 11:57:51 +0200

    Makes SignItem entities extends AbstractSignItem

--- src/Entity/SectorSignItem.php
+++ src/Entity/SectorSignItem.php
@@ -8,49 +8,15 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ORM\Entity(repositoryClass=SectorSignItemRepository::class)
  */
-class SectorSignItem
+class SectorSignItem extends AbstractSignItem
 {
     public const BLUE = 'blue';
     public const GREY = 'grey';
 
     /**
-     * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
-     */
-    private $id;
-
-    /**
-     * @ORM\Column(type="string", length=60)
-     */
-    private $label;
-
-    /**
      * @ORM\Column(type="string", length=5)
      */
     private $color;
-
-    /**
-     * @ORM\Column(type="boolean")
-     */
-    private $isActive;
-
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
-    public function getLabel(): ?string
-    {
-        return $this->label;
-    }
-
-    public function setLabel(string $label): self
-    {
-        $this->label = $label;
-
-        return $this;
-    }
 
     public function getColor(): ?string
     {
@@ -60,18 +26,6 @@ class SectorSignItem
     public function setColor(string $color): self
     {
         $this->color = $color;
-
-        return $this;
-    }
-
-    public function getIsActive(): ?bool
-    {
-        return $this->isActive;
-    }
-
-    public function setIsActive(bool $isActive): self
-    {
-        $this->isActive = $isActive;
 
         return $this;
     }
