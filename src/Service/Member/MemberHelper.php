1f378014f50ba29b5ec3ff07a797fbe723a225cf
Pierre Gaimard <pierre@gaimard-family.fr>
2021-10-05 10:29:52 +0200

    Add MemberHelper service

---
+++ src/Service/Member/MemberHelper.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Service\Member;
+
+use App\Entity\Event;
+use App\Entity\Member;
+use Doctrine\ORM\EntityManagerInterface;
+
+class MemberHelper
+{
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+    ) {
+    }
+
+    /**
+     * @param Member $member
+     */
+    public function setDefaultMemberEvents(Member $member): void
+    {
+        $events = $this->entityManager->getRepository(Event::class)->findAll();
+
+        foreach ($events as $event) {
+            $member->addEvent($event);
+        }
+    }
+}
