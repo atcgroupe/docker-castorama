6e569fb58d40d2fcee2b2d16c5017d21ac277f80
Pierre Gaimard <pierre@gaimard-family.fr>
2021-10-26 11:15:41 +0200

    Add encrypted passwords to users fixtures

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -15,7 +15,6 @@ class UserFixtures extends Fixture implements DependentFixtureInterface
     public const PASSWORD = 'password';
 
     public function __construct(
-        private UserPasswordHasherInterface $passwordHasher,
         private CsvReader $csvReader,
     ) {
     }
@@ -46,16 +45,16 @@ class UserFixtures extends Fixture implements DependentFixtureInterface
 
         // Other users
         $otherUsersData = [
-            [User::SIEGE, 'pass', User::ROLE_CUSTOMER_ADMIN],
-            [User::ATC, 'pass', User::ROLE_COMPANY_ADMIN],
-            [User::API, 'pass', User::ROLE_API],
+            [User::SIEGE, '$2y$13$2Ua2zF536TC.sXGzI4tl.e1XVdC9njoi1i1IEXs4sHyLn6u7bkS5G', User::ROLE_CUSTOMER_ADMIN],
+            [User::ATC, '$2y$13$M10IrrBjgRspS8ktrDvj6e06coR0sAm0gQXWUnIdwwLsSp5mARiFa', User::ROLE_COMPANY_ADMIN],
+            [User::API, '$2y$13$Aak6GQHVlv.q7bQxIU066.FbxWprK7/EpWUKdYI6Td72wfMOTEL5e', User::ROLE_API],
         ];
 
         foreach ($otherUsersData as $entry) {
             $user = new User();
 
             $user->setUsername($entry[0]);
-            $user->setPassword($this->passwordHasher->hashPassword($user, $entry[1]));
+            $user->setPassword($entry[1]);
             $user->setRoles([$entry[2]]);
             $user->setIsActive(true);
 
