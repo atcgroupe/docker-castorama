f73876c0b389be519cdd3f16928a8374067d6b34
Pierre Gaimard <pierre@gaimard-family.fr>
2021-11-04 14:26:12 +0100

    Add missing unique constraint in AisleSmallOrderSign

--- src/Entity/AisleSmallOrderSign.php
+++ src/Entity/AisleSmallOrderSign.php
@@ -4,10 +4,16 @@ namespace App\Entity;
 
 use App\Repository\AisleSmallOrderSignRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass=AisleSmallOrderSignRepository::class)
  * @ORM\HasLifecycleCallbacks()
+ * @UniqueEntity(
+ *     fields={"order", "aisleNumber"},
+ *     errorPath="aisleNumber",
+ *     message="Un panneau allée avec ce numéro existe déjà dans cette commande"
+ * )
  */
 class AisleSmallOrderSign extends AbstractAisleOrderSign
 {
