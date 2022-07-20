8f8db2d4428b95417968ab3fc068cd6d917c8536
Pierre Gaimard <pierre@gaimard-family.fr>
2022-07-20 09:04:00 +0200

    improve MaterialSectorOrderSign

--- src/Entity/MaterialSectorOrderSign.php
+++ src/Entity/MaterialSectorOrderSign.php
@@ -5,32 +5,43 @@ namespace App\Entity;
 use App\Repository\MaterialSectorOrderSignRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Serializer\Annotation\SerializedName;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=MaterialSectorOrderSignRepository::class)
+ * @ORM\HasLifecycleCallbacks()
  * @UniqueEntity(
- *     fields={"order", "aisleNumber"},
+ *     fields={"order", "aisleNumber", "alignment"},
  *     errorPath="aisleNumber",
- *     message="Un panneau allée avec ce numéro existe déjà dans cette commande"
+ *     message="Un panneau allée avec ce numéro et cet alignement existe déjà dans cette commande"
  * )
  */
 class MaterialSectorOrderSign extends AbstractOrderSign
 {
     private const TYPE = 'materialSector';
 
+    public const ALIGN_LEFT = 'left';
+    public const ALIGN_RIGHT = 'right';
+    public const ALIGN_ALL = 'all'; // Used in form to create 2 signs (one left + one right)
+
     /**
      * @ORM\Column(type="smallint")
+     * @Groups({"api_json_data"})
      */
     private $aisleNumber;
 
     /**
      * @ORM\Column(type="string", length=10)
+     * @Groups({"api_json_data"})
      */
     private $alignment;
 
     /**
      * @ORM\ManyToOne(targetEntity=MaterialSectorSignItem::class)
      * @ORM\JoinColumn(nullable=false)
+     * @Assert\NotBlank(message="Vous devez sélectionner au moins un produit.")
      */
     private $item1;
 
@@ -43,6 +54,11 @@ class MaterialSectorOrderSign extends AbstractOrderSign
      * @ORM\ManyToOne(targetEntity=MaterialSectorSignItem::class)
      */
     private $item3;
+
+    /**
+     * @Assert\NotBlank(message="Vous devez sélectionner la catégorie.")
+     */
+    private $category;
 
     public function getAisleNumber(): ?int
     {
@@ -104,14 +120,86 @@ class MaterialSectorOrderSign extends AbstractOrderSign
         return $this;
     }
 
+    /**
+     * @ORM\PostLoad()
+     */
+    public function initializeCategory()
+    {
+        $this->setCategory(($this->getItem1() !== null) ? $this->getItem1()->getCategory() : null);
+    }
+
+    /**
+     * @return MaterialSectorSignItemCategory|null
+     */
+    public function getCategory(): ?MaterialSectorSignItemCategory
+    {
+        return $this->category;
+    }
+
+    /**
+     * @param MaterialSectorSignItemCategory|null $category
+     */
+    public function setCategory(?MaterialSectorSignItemCategory $category): void
+    {
+        $this->category = $category;
+    }
+
     public static function getType(): string
     {
         return self::TYPE;
     }
 
+    /**
+     * @return string
+     * @Groups({"api_json_data"})
+     */
     public function getFileName(): string
     {
-        // TODO: Implement getFileName() method.
-        return 'xxxxx';
+        return sprintf(
+            'COMMANDE %s PANNEAU ALLEE EXTERIEUR %s %sEX.xml',
+            $this->getOrderId(),
+            $this->getAisleNumber(),
+            $this->getQuantity()
+        );
+    }
+
+    /**
+     * @return string
+     * @Groups({"api_json_data"})
+     * @SerializedName("sectorLabel")
+     */
+    public function getSectorLabel(): string
+    {
+        return (null === $this->getCategory()) ? '' : $this->getCategory()->getLabel();
+    }
+
+    /**
+     * @return string
+     * @Groups({"api_json_data"})
+     * @SerializedName("item1Label")
+     */
+    public function getItem1Label(): string
+    {
+        return (null === $this->getItem1()) ? '' : $this->getItem1()->getLabel();
+    }
+
+    /**
+     * @return string
+     * @Groups({"api_json_data"})
+     * @SerializedName("item2Label")
+     */
+    public function getItem2Label(): string
+    {
+        return (null === $this->getItem2()) ? '' : $this->getItem2()->getLabel();
+    }
+
+    /**
+     * @return string
+     * @Groups({"api_json_data"})
+     * @SerializedName("item3Label")
+     */
+    public function getItem3Label(): string
+    {
+        return (null === $this->getItem3()) ? '' : $this->getItem3()->getLabel();
     }
 }
