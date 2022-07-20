bf79eda2dd2c2795c188aa67b84875393c6deb09
Pierre Gaimard <pierre@gaimard-family.fr>
2022-07-20 08:47:51 +0200

    Add new sign edit js management

---
+++ assets/scripts/sign/material_sector_sign.js
@@ -0,0 +1,4 @@
+import { MaterialSectorSignHelper } from "./MaterialSectorSignHelper";
+const form = document.getElementsByTagName('form')[0];
+
+new MaterialSectorSignHelper(form.getAttribute('name'));
