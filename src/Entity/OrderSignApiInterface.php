52582c0bce3b1c4300db03ff8e0d865227c0afbc
Pierre Gaimard <pierre@gaimard-family.fr>
2022-07-22 11:19:44 +0200

    move methods to VariableOrderSignApiInterface

--- src/Entity/OrderSignApiInterface.php
+++ src/Entity/OrderSignApiInterface.php
@@ -5,29 +5,6 @@ namespace App\Entity;
 interface OrderSignApiInterface
 {
     /**
-     * Returns the Callas builder name that is used to apply content on pdf file.
-     *
-     * @return string
-     */
-    public function getSwitchBuilder(): string;
-
-    /**
-     * Returns the Callas pdf template name that is used to generate production file.
-     *
-     * Note: the name is returned without file extension.
-     *
-     * @return string
-     */
-    public function getSwitchTemplate(): string;
-
-    /**
-     * Used for Enfocus Switch API
-     *
-     * @return string
-     */
-    public function getData(): string;
-
-    /**
      * Used for Enfocus Switch API
      *
      * @return int
@@ -40,4 +17,11 @@ interface OrderSignApiInterface
      * @return string
      */
     public function getFileName(): string;
+
+    /**
+     * Returns the type of the sign (VARIABLE or FIXED)
+     *
+     * @return string
+     */
+    public function getSwitchSignType(): string;
 }
