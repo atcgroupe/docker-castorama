f09f510c5a2b12ea73e227afae4bc2a1812215b5
Pierre Gaimard <pierre@gaimard-family.fr>
2022-07-22 11:23:22 +0200

    makes MaterialAlgecoSignItem extends AbstractSignItem

--- src/Entity/MaterialAlgecoSignItem.php
+++ src/Entity/MaterialAlgecoSignItem.php
@@ -8,51 +8,6 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ORM\Entity(repositoryClass=MaterialAlgecoSignItemRepository::class)
  */
-class MaterialAlgecoSignItem
+class MaterialAlgecoSignItem extends AbstractSignItem
 {
-    /**
-     * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
-     */
-    private $id;
-
-    /**
-     * @ORM\Column(type="string", length=30)
-     */
-    private $label;
-
-    /**
-     * @ORM\Column(type="boolean")
-     */
-    private $isActive;
-
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
-    public function getLabel(): ?string
-    {
-        return $this->label;
-    }
-
-    public function setLabel(string $label): self
-    {
-        $this->label = $label;
-
-        return $this;
-    }
-
-    public function getIsActive(): ?bool
-    {
-        return $this->isActive;
-    }
-
-    public function setIsActive(bool $isActive): self
-    {
-        $this->isActive = $isActive;
-
-        return $this;
-    }
 }
