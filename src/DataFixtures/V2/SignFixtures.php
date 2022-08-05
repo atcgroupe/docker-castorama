0fbe7591c0a3fc03819a1886741bd9438212423b
Pierre Gaimard <pierre@gaimard-family.fr>
2022-08-05 11:38:17 +0200

    add new sign fixtures

--- src/DataFixtures/V2/SignFixtures.php
+++ src/DataFixtures/V2/SignFixtures.php
@@ -7,6 +7,7 @@ use App\Entity\AbstractSign;
 use App\Entity\MaterialAlgecoOrderSign;
 use App\Entity\MaterialDirOrderSign;
 use App\Entity\MaterialSectorOrderSign;
+use App\Entity\MaterialServiceOrderSign;
 use App\Entity\Sign;
 use App\Service\Fixtures\AppVersionFixturesGroup;
 
@@ -56,6 +57,20 @@ class SignFixtures extends AbstractSignFixtures
                 self::WEIGHT => 1.33,
                 self::BUILDER => 'MaterialAlgecoSignBuilder',
                 self::TEMPLATE => 'MaterialAlgecoSignTemplate',
+                self::PRICE => 40.00,
+                self::CUSTOMER_REF => 'xxxxxx',
+                self::CATEGORY => AbstractSign::CATEGORY_OUTDOOR,
+            ],
+            [
+                self::ID => 7,
+                self::CLASS_NAME => MaterialServiceOrderSign::class,
+                self::TYPE => MaterialServiceOrderSign::getType(),
+                self::IMAGE => 'material_service_order_sign.jpg',
+                self::TITLE => 'Panneau services',
+                self::DESCRIPTION => 'Format: 1050x525mm<br>Impression: Recto<br>Matière: Dibond 3MM',
+                self::WEIGHT => 2.09,
+                self::BUILDER => 'none',
+                self::TEMPLATE => 'MaterialServiceSignTemplate',
                 self::PRICE => 40.00,
                 self::CUSTOMER_REF => 'xxxxxx',
                 self::CATEGORY => AbstractSign::CATEGORY_OUTDOOR,
