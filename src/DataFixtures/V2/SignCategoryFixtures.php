2b85cc041ea1118ae95a66135184547a894ba1b7
Pierre Gaimard <pierre@gaimard-family.fr>
2022-08-08 11:09:26 +0200

    Refactor Sign fixtures

---
+++ src/DataFixtures/V2/SignCategoryFixtures.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\DataFixtures\V2;
+
+use App\Entity\SignCategory;
+use App\Service\Fixtures\AppVersionFixturesGroup;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class SignCategoryFixtures extends Fixture implements FixtureGroupInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $data = [
+            1 => 'Signalétique interieure',
+            2 => 'Cour des matériaux',
+        ];
+
+        foreach ($data as $key => $label) {
+            $category = new SignCategory();
+            $category->setLabel($label);
+
+            $manager->persist($category);
+            $this->setReference($key, $category);
+        }
+
+        $manager->flush();
+    }
+
+    public static function getGroups(): array
+    {
+        return [
+            AppVersionFixturesGroup::V2,
+        ];
+    }
+}
